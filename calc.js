ae389c6344d0072556b5d5c1d54fec4b5c8dac3c
Curtis West <curtis@curtiswest.net>
2017-09-30 01:15:54 +1000

    Improve calculation logic

--- calc.js
+++ calc.js
@@ -3,39 +3,31 @@ $("#discount").on('input keyup', update_discount_calcs);
 $("#cash").on('input keyup', update_discount_calcs);
 
 function update_discount_calcs() {
-    price = $("#petrolprice").val();
-    discount = $("#discount").val();
-    cash = $("#cash").val();
+    price = parseInt($("#petrolprice").val());
+    discount = parseInt($("#discount").val());
+    cash = parseInt($("#cash").val());
 
     litres = calc_litres(price, discount, cash);
     spend = calc_spend(price, litres);
 
-    if (isFinite(litres)) {
+    if (isFinite(litres) && isFinite(spend)) {
         $(".result_litres").html(litres.toFixed(2));
+        $(".result_spend").html(spend.toFixed(2));
 
         without_discount = calc_litres(price, 0, cash);
         petrol_gained = litres - without_discount;
-        if (isFinite(petrol_gained)) {
-            $(".petrol_gained").html(petrol_gained.toFixed(2));
-        }
-    }
-    if (isFinite(spend)) {
-        $(".result_spend").html(spend.toFixed(2));
+        $(".petrol_gained").html(petrol_gained.toFixed(2));
+    } else {
+        $(".result_litres").html("?");
+        $(".result_spend").html("?");
+        $(".petrol_gained").html("?");
     }
 }
 
 function calc_litres(price, discount, cash) {
-    if (isNaN(price) || isNaN(discount) || isNaN(cash)) {
-        return 0
-    } else {
-        return (cash * 100) / (price - discount);
-    }
+    return (cash * 100) / (price - discount);
 }
 
 function calc_spend(price, litres) {
-    if (isNaN(price) || isNaN(litres)) {
-        return 0
-    } else {
-        return (price * litres) / 100;
-    }
+    return (price * litres) / 100;
 }
